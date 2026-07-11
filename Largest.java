77c93d0a9632a2b05ff3cf614afb2db335ff0112
vyshnavi1235 <gandlavyshu86@gmail.com>
2026-07-11 12:05:31 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,12 @@
+class Largest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println("A is largest");
+        } else {
+            System.out.println("B is largest");
+        }
+    }
+}
