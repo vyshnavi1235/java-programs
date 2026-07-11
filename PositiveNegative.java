4964cd5a7af77c1b82810fe2bbcb2e84373acaf8
vyshnavi1235 <gandlavyshu86@gmail.com>
2026-07-11 12:04:40 +0530

    Create PositiveNegative.java

---
+++ PositiveNegative.java
@@ -0,0 +1,11 @@
+class PositiveNegative {
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
