3b6d389314097be419d2c9119a9ae0b2c0cbeabe
vyshnavi1235 <gandlavyshu86@gmail.com>
2026-07-11 12:14:04 +0530

    Create Ternary.java

---
+++ Ternary.java
@@ -0,0 +1,9 @@
+class Ternary {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int max = (a > b) ? a : b;
+        System.out.println("Max: " + max);
+    }
+}
