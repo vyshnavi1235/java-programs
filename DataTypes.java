01a6a9d93fb61581615246c593f5c347b83d4c47
vyshnavi1235 <gandlavyshu86@gmail.com>
2026-07-11 12:01:33 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+class DataTypes {
+    public static void main(String[] args){
+        int a = 10;
+        float b = 5.5f;
+        double c = 99.99;
+        char d = 'A';
+        boolean e = true;
+        String f = "vyshu"
+        System.out.println("Integer value: " + a);
+        System.out.println("Float value: " + b);
+        System.out.println("Double value: " + c);
+        System.out.println("Character value: " + d);
+        System.out.println("Boolean value: " + e);
+        System.out.println("String value: " + e);
+    }
+}
