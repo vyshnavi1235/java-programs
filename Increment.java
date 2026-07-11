9dcc8421ca8423a3cbce6bb38469842b420e4976
vyshnavi1235 <gandlavyshu86@gmail.com>
2026-07-11 12:12:53 +0530

    Create Increment.java

---
+++ Increment.java
@@ -0,0 +1,11 @@
+class Increment {
+    public static void main(String[] args) {
+        int a = 5;
+
+        a++; 
+        System.out.println(a);
+
+        a--; 
+        System.out.println(a);
+    }
+}
