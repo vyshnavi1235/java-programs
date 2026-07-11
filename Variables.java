1293550ff5b19b523da2199f702b04016cd69755
vyshnavi1235 <gandlavyshu86@gmail.com>
2026-07-11 11:56:13 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,10 @@
+class Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        System.out.println("A value: " + a);
+        System.out.println("B value: " + b);
+        System.out.println("Sum: " + (a + b));
+    }
+}
